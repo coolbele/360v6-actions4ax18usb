fa9b09aa46ddc8813c14ae58765139039a46ac58
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-07-06 12:40:53 +0800

    Create diy-part1-1.sh

---
+++ diy-part1-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 或者使用下面的命令修改，注意 Makefile 文件的路径。
+# sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/qualcommax/Makefile
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+# sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# echo 'src-git roacn https://github.com/roacn/openwrt-packages' >>feeds.conf.default
