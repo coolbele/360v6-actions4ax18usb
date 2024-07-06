42b8377a3b7d53c31ab71263b170e308ee8dfb12
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-07-06 11:10:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 # sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+echo 'src-git roacn https://github.com/roacn/openwrt-packages' >>feeds.conf.default
